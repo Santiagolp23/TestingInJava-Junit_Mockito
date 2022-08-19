337759ed007425a92abb57a7bf633c3175e48900
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-08-18 21:56:52 -0500

    changed testing method for StringUtil for more efficient one

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -3,8 +3,15 @@ package com.platzi.javatests.util;
 public class StringUtilTest {
     public static void main(String[] args) {
 
-        String result = StringUtil.repeat("Hello " , 3);
-        System.out.println(result);
+        String result = StringUtil.repeat("Hello ", 3);
+        if (!result.equals("Hello Hello Hello ")) {
+            System.out.println("ERROR");
+        }
+
+        String result2 = StringUtil.repeat("Hello ", 1);
+        if (!result2.equals("Hello ")) {
+            System.out.println("ERROR");
+        }
     }
 
 }
