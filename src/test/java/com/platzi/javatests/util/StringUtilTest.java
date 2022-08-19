9c2587f5c26113d3b4afd2a6e884dc40dc509ca5
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-08-18 21:47:44 -0500

    Initialization of Maven project + first test of StringUtil class

---
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -0,0 +1,10 @@
+package com.platzi.javatests.util;
+
+public class StringUtilTest {
+    public static void main(String[] args) {
+
+        String result = StringUtil.repeat("Hello " , 3);
+        System.out.println(result);
+    }
+
+}
