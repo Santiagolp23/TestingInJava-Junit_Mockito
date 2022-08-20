95db355b48e30a304a22870ef23de126f27cef94
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-08-19 21:49:39 -0500

    Made the StringUtilTest a real @test using JUnit

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -5,12 +5,19 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class StringUtilTest {
-    public static void main(String[] args) {
 
-        assertEquals(StringUtil.repeat("Hello ", 3), "Hello Hello Hello ");
-
+    @Test
+    public void repeatOnce() {
         assertEquals(StringUtil.repeat("Hello ", 1), "Hello ");
-
     }
 
+    @Test
+    public void repeatThreeTimes() {
+        assertEquals(StringUtil.repeat("Hello ", 3), "Hello Hello Hello ");
+    }
+
+    @Test
+    public void repeatZeroTimes() {
+        assertEquals(StringUtil.repeat("Hello ", 0), "");
+    }
 }
