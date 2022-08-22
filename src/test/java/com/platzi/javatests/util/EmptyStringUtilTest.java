e232c06214ec5497670e0b05fbbe609a08f8f3e3
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-08-21 22:53:30 -0500

    check if string is empty or null + corresponding tests

---
+++ src/test/java/com/platzi/javatests/util/EmptyStringUtilTest.java
@@ -0,0 +1,28 @@
+package com.platzi.javatests.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmptyStringUtilTest {
+
+    @Test
+    public void isNotEmpty() {
+        assertFalse(EmptyStringUtil.isEmpty("ABCD"));
+    }
+
+    @Test
+    public void isEmptyWithSpace() {
+        assertTrue(EmptyStringUtil.isEmpty(" "));
+    }
+
+    @Test
+    public void isEmpty() {
+        assertTrue(EmptyStringUtil.isEmpty(""));
+    }
+
+    @Test
+    public void isEmptyWhenNull() {
+        assertTrue(EmptyStringUtil.isEmpty(null));
+    }
+}
