7ab70d497abbd08ad1d90735e53f5f63aefaef89
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-08-21 22:29:57 -0500

    cleanup of repeated variable declaration in each test

--- src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/com/platzi/javatests/payments/PaymentProcessorTest.java
@@ -1,19 +1,18 @@
 package com.platzi.javatests.payments;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.mockito.internal.configuration.injection.filter.MockCandidateFilter;
 
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+    PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+    PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
     @Test
     public void correctlyMadePayment() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertTrue(paymentProcessor.makePayment(1000));
 
@@ -21,10 +20,7 @@ public class PaymentProcessorTest {
 
     @Test
     public void wronglyMadePayment() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any())).thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertFalse(paymentProcessor.makePayment(1000));
 
