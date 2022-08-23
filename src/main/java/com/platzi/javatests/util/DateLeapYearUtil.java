1c134c83c3efed0ba6863e55033b653164ee01c7
Santiago Lopez <santiagolopezpardo@gmail.com>
2022-08-22 22:40:33 -0500

    refactor - implemented the whole function inline with return

--- src/main/java/com/platzi/javatests/util/DateLeapYearUtil.java
+++ src/main/java/com/platzi/javatests/util/DateLeapYearUtil.java
@@ -2,14 +2,6 @@ package com.platzi.javatests.util;
 
 public class DateLeapYearUtil {
     public static boolean isLeapYear(int year) {
-        if (year % 4 == 0 && year % 100 != 0) {
-            return true;
-        }
-
-        if (year % 400 == 0) {
-            return true;
-        }
-
-        return false;
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 }
